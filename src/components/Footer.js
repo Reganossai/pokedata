664089ddb57d5b23cb246ed5d986e797d077d617
Reagan <ossaireagano@gmail.com>
2023-02-08 11:30:46 +0100

    landing page pokedata app

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
+
+const Footer = () => {
+    return(
+        <div className="footer-div">
+            <div className="first-col">
+                <h4><FontAwesomeIcon icon={faCopyright} className="copyright"/>pokedata 2023</h4>
+                <h4>Privacy</h4>
+                <h4>Terms and Conditions</h4>
+            </div>
+
+            <div className="second-col">
+                <h4>Affiliates</h4>
+                <h4>/r/PokémonTCG · Discord</h4>
+                <h4>/r/PKMNTCGTrades · Discord</h4>
+            </div>
+            
+            <div className="third-col">
+                <h4>Contact</h4>
+                <h4>Reddit</h4>
+                <h4>Discord</h4>
+                <h4>Email</h4>
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBuilding} from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+const Home = () => {
+    return(
+        <div className="body-container">
+            <div className="logo">
+                <img src="https://www.pokedata.io/static/media/pd_logo_color_black.782acf0a.png" alt="logo"/>
+            </div>
+            <div className="icons">
+                <FontAwesomeIcon icon={faEnvelope} className="iconic"/>
+                <FontAwesomeIcon icon={faBuilding} className="iconic"/>
+            </div>
+            <div className="body-text">
+                <h1 className="we-provide">We provide complete sales</h1>
+                <h1 className="prices-on">prices on Pokemon cards and</h1>
+                <h1 className="products">products.</h1>
+                <h4 className="analyze-trends">Analyze trends and statistics for CGC, PSA and Raw card values</h4>
+                <h4 className="as-well-as">as well as Sealed Products. Create a portfolio to track your</h4>
+                <h4 className="collections-worth">collection’s worth or monitor your Pokemon investments.</h4>
+            </div>
+            <div className="images">
+                <img src="https://www.pokedata.io/static/media/cards_diagonal.4c833803.png" alt="first img"/>
+                    </div>
+
+                    <div className="button-div">
+<button type="button"><h3>Sets</h3></button>
+<button type="button"><h3>Sealed Products</h3></button>
+<button type="button"><h3>Portfolio Tracker</h3></button>
+                    </div>
+
+                    <div className="search">
+                        <input type="text" placeholder="Unsure of Set? Search Card Names" />
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className="search-btn"/>
+                    </div>
+<hr/>
+        </div>
+
+
+
+
+
+    );
+
+
+
+}
+
+export default Home;
